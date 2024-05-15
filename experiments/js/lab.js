fda5eec20f4b17ec6b5ff303b1253eb6c7292cc2
rrichm <rylindvic@gmail.com>
2024-05-15 16:01:09 -0700

    lab 10

---
+++ experiments/js/lab.js
@@ -0,0 +1,7 @@
+$("#my-button").click()
+var name = prompt ("Name please:")
+$('#title').html('Hello'  + name)
+$("#my-button").click(getNameAndDisplay);
+// $("#my-button").click(function(){
+// console.log("click")
+// });
