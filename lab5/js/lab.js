afb76d179e54e51475c4ac5516fb1e4e2ac4220e
rrichm <rylindvic@gmail.com>
2024-05-28 16:03:48 -0700

    lab 14

--- lab5/js/lab.js
+++ lab5/js/lab.js
@@ -1,27 +1,25 @@
 function main() {
-  console.log("Main function started.");
-/* 
-* Author: Rylind Richmond <rrichmon@ucsc.edu>
-* Created: 24 April 2024
-* License: Rylind Richmond 
-*/
-// Define Variables 
-make="Ford";
-model = "Taurus";
-color = "Rusty";
-year = 1995; 
-// calculate
-age = 2019 - year; 
-
-// output 
-document.writeln ("make:" + make + "<br>");
-document.writeln("Model:" + model + "<br>");
-document.writeln("Color: " + color + "<br>");
-document.writeln("Year: " + year + "<br>");
-document.writeln("Age: " + age + "years<br>");
-}
-
- 
-
-
-
+    console.log("Main function started.");
+  /* 
+  * Author: Rylind Richmond <rrichmon@ucsc.edu>
+  * Created: 24 April 2024
+  * License: Rylind Richmond 
+  */
+  // Define Variables 
+  make = "Ford";
+  model = "Taurus";
+  color = "Rusty";
+  year = 1995; 
+  // calculate
+  age = 2019 - year; 
+  
+  // output 
+  document.writeln ("make:" + make + "<br>");
+  document.writeln("Model:" + model + "<br>");
+  document.writeln("Color: " + color + "<br>");
+  document.writeln("Year: " + year + "<br>");
+  document.writeln("Age: " + age + " years<br>");
+  }
+  
+   
+  
